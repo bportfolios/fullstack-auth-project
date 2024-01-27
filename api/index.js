9c464f11bd0f2c370252cbd0271b10f33c2a3193
Bipika Amatya <bipika.amatya@bithouse.fi>
2024-01-28 00:54:22 +0200

    model,route,controller,error middleware added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,32 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.VITE_MONGO)
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((e) => {
+    console.log(`DB Error:${e}`);
+  });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server listen on port 3000");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({ success: false, message, statusCode });
 });
