133dee1ac54ed5737a95d5cf45e78134d8e9eff6
Bipika Amatya <bipika.amatya@bithouse.fi>
2024-01-30 01:37:52 +0200

    reduxjs/toolkit; signin,signup

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const [notification, setNotification] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(signInStart());
+    await axios
+      .post("/api/auth/signin", formData)
+      .then((res) => {
+        dispatch(signInSuccess(res));
+        setNotification(res.data.message);
+        navigate("/");
+      })
+      .catch((err) => {
+        dispatch(signInFailure(err.response.data.message));
+        setNotification(err.response.data.message);
+      });
+  };
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Don't have an account ?</p>
+        <Link to="/signup">
+          <span className="text-blue-500">Sign Up</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{notification}</p>
+    </div>
+  );
 }
 
-export default SignIn
+export default SignIn;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [notification, setNotification] = useState("");
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    await axios
+      .post("/api/auth/signup", formData)
+      .then((res) => {
+        setLoading(false);
+        setNotification(res.data.message);
+        navigate("/");
+      })
+      .catch((e) => {
+        setLoading(false);
+        setNotification(e.message);
+      });
+  };
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-200 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account ?</p>
+        <Link to="/signin">
+          <span className="text-blue-500">Sign In</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{notification}</p>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
